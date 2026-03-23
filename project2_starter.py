2e9fda81fd084ba81ec13803f3e33723e3697073
lynn van <lynnvan@lynns-MacBook-Air.local>
2026-03-23 19:15:29 -0400

    name

--- project2_starter.py
+++ project2_starter.py
@@ -1,10 +1,10 @@
 # SI 201 HW4 (Library Checkout System)
-# Your name:
-# Your student id:
-# Your email:
+# Your name: lynn van
+# Your student id: 15287597
+# Your email: lynnvan@umich.edu
 # Who or what you worked with on this homework (including generative AI like ChatGPT):
 # If you worked with generative AI also add a statement for how you used it.
-# e.g.:
+# e.g.: ashka patel
 # Asked ChatGPT for hints on debugging and for suggestions on overall code structure
 #
 # Did your use of GenAI on this assignment align with your goals and guidelines in your Gen AI contract? If not, why?
